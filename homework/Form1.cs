91e48262dae4e785ce758c8337b7bad3aa7b7495
Jerry-1092010180 <jerry1092010180@gmail.com>
2026-03-01 00:36:45 +0800

    C#
    C#

---
+++ homework/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 变色窗体
+{
+    public partial class Form1 : Form
+    {
+        private bool isRed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 按钮_Click(object sender, EventArgs e)
+        {
+            if (!isRed)
+            {
+                this.BackColor = Color.Red;
+                isRed = true;
+            }
+            else
+            {
+                this.BackColor = Color.Blue;
+                isRed = false;
+            }
+        }
+    }
+}
